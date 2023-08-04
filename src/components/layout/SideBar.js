5443bfc44e16d1c631d566fbab0d4f9f1e9353ed
jeongboram <pink0913@nate.com>
2023-08-04 12:08:48 +0900

    feat: second commit

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,85 +1,116 @@
-import React from "react";
+import React from 'react';
 
 function SideBar() {
-    const onSubmit = (e) => {
-        e.preventDefault();
-        console.log("on submit");
-    };
-    return (
-        <>
-            <div className="sidebar-section">
-                <div className="head-title">
-                    <h1>LG Channels Monotoring Dashboard</h1>
-                    <button className="btn-refresh">새로고침</button>
-                </div>
-                <div className="box-total-issue">
-                    <h2>Total number of issue</h2>
-                    <span className="time">2023-07-13 10:10:00</span>
-                    <strong>145,555</strong>
-                </div>
-                <div className="box-summary-tabs">
-                    <div className="ui-tab-type01">
-                        <div className="ui-tab-menu">
-                            <a href="javascript:void(0);">Service</a>
-                            <a href="javascript:void(0);">Contents</a>
-                            <a href="javascript:void(0);">Statistics</a>
-                        </div>
-                        <div className="ui-tab-contents">
-                            <div className="tab-box">Service Tab Contents</div>
-                            <div className="tab-box">Contents Tab Contents</div>
-                            <div className="tab-box">
-                                Statistics Tab Contents
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                <div className="box-search">
-                    <h2>Search for Issue Code</h2>
-                    <div className="field-input-search">
-                        <form action="#" onSubmit={onSubmit}>
-                            <i></i>
-                            <input type="text" placeholder="Search all Code" />
-                        </form>
-                    </div>
-                </div>
-                <div className="box-issus-inquiry">
-                    <h2>Issue Inquiry</h2>
-                    <table>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>text@lg.com</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>text@lg.com</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>text@lg.com</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>text@lg.com</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>text@lg.com</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-                <footer>
-                    <p>
-                        This data is from the HE Service Operations team
-                        Available
-                        <br />
-                        based on LG Smart TV automation data.
-                    </p>
-                </footer>
-            </div>
-        </>
-    );
+	const onSubmit = (e) => {
+		e.preventDefault();
+		console.log('on submit');
+	};
+	return (
+		<>
+			<div className="sidebar-section">
+				<div className="head-title">
+					<h1>
+						LG Channels
+						<br />
+						Issue Sensing System
+					</h1>
+					<button className="btn-refresh">새로고침</button>
+				</div>
+				<div className="box-total-issue">
+					<h2>Total Issues</h2>
+					<span className="time">2023-07-13 10:10:00</span>
+					<strong>145,555</strong>
+				</div>
+				<div className="box-summary-tabs">
+					<div className="ui-tab-type01">
+						<div className="ui-tab-menu">
+							<a href="javascript:void(0);" className="on">
+								Service
+							</a>
+							<a href="javascript:void(0);">Contents</a>
+							<a href="javascript:void(0);">Statistics</a>
+						</div>
+						<div className="ui-tab-contents">
+							<div className="tab-box">
+								<div className="contents-service">
+									<div className="line">
+										<h3>Issue Count</h3>
+										<div className="num-summary">
+											<strong>1,842</strong>
+											<span className="new">5</span>
+										</div>
+									</div>
+									<div className="line">
+										<h3>Unaddressed</h3>
+										<div className="num-summary">
+											<strong>76</strong>
+										</div>
+									</div>
+									<div className="line">
+										<h3>Precessing</h3>
+										<div className="num-summary">
+											<strong>95</strong>
+										</div>
+									</div>
+									<div className="ui-chart-container">
+										<h3>
+											Status of issues by time zone <span>(Based on UTC)</span>
+										</h3>
+										<div className="chart-wrap">chart area</div>
+									</div>
+								</div>
+							</div>
+							<div className="tab-box">Contents Tab Contents</div>
+							<div className="tab-box">Statistics Tab Contents</div>
+						</div>
+					</div>
+				</div>
+				<div className="box-search">
+					<h2>Search for Issue Code</h2>
+					<div className="field-input-search">
+						<form action="#" onSubmit={onSubmit}>
+							<i></i>
+							<input type="text" placeholder="Search" />
+						</form>
+					</div>
+				</div>
+				<div className="box-issus-inquiry">
+					<h2>Issue Inquiry</h2>
+					<table>
+						<tbody>
+							<tr>
+								<th scope="row">구분1</th>
+								<td>text@lg.com</td>
+							</tr>
+							<tr>
+								<th scope="row">구분1</th>
+								<td>text@lg.com</td>
+							</tr>
+							<tr>
+								<th scope="row">구분1</th>
+								<td>text@lg.com</td>
+							</tr>
+							<tr>
+								<th scope="row">구분1</th>
+								<td>text@lg.com</td>
+							</tr>
+							<tr>
+								<th scope="row">구분1</th>
+								<td>text@lg.com</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+				<footer>
+					<p>
+						This data is from the HE Service Operations team Available
+						<br />
+						based on LG Smart TV automation data.
+					</p>
+				</footer>
+			</div>
+		</>
+	);
 }
 
 export default SideBar;
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,18 +1,484 @@
-import React from "react";
-import SideBar from "../components/layout/SideBar";
+import React from 'react';
+import SideBar from '../components/layout/SideBar';
 
 const Dashboard = () => {
-    return (
-        <>
-            <div className="main-container">
-                <div className="inner">
-                    <SideBar />
-                    <div className="contents-section">
-                        dashboard contents section
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+	return (
+		<>
+			<div className="main-container">
+				<div className="inner">
+					<SideBar />
+					<div className="contents-section">
+						<div className="grid-vertical-two">
+							<div className="grid-left">
+								<div className="box-worldmap">
+									<div className="head">
+										<div className="title">
+											<h1>Monitoring World Map</h1>
+										</div>
+										<div className="ui-components-box">
+											<div className="ui-dropdown-type01">
+												<select>
+													<option>all Region</option>
+													<option>all Region</option>
+													<option>all Region</option>
+												</select>
+											</div>
+											<div className="ui-dropdown-type01">
+												<select>
+													<option>all Region</option>
+													<option>all Region</option>
+													<option>all Region</option>
+												</select>
+											</div>
+											<div className="ui-dropdown-type01">
+												<select>
+													<option>all Region</option>
+													<option>all Region</option>
+													<option>all Region</option>
+												</select>
+											</div>
+											<button className="btn-search">Search</button>
+										</div>
+									</div>
+									<div className="ui-chart-container"></div>
+								</div>
+								<div className="grid-column-2">
+									<div className="grid-left">
+										<div className="box-appIssues box-tbl">
+											<div className="head">
+												<div className="title">
+													<h2>LG Channels App Issues</h2>
+													<div className="ui-tooltip">
+														<i></i>
+														<div className="tooltip">tooltip</div>
+													</div>
+												</div>
+											</div>
+											<div className="ui-table-default">
+												<table>
+													<colgroup>
+														<col width={58}></col>
+														<col width={44}></col>
+														<col width={47}></col>
+														<col width={310}></col>
+													</colgroup>
+													<thead>
+														<tr>
+															<th scope="col">Date</th>
+															<th scope="col">Country</th>
+															<th scope="col">Platform</th>
+															<th scope="col">Issue Contents</th>
+														</tr>
+													</thead>
+													<tbody>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+										</div>
+									</div>
+									<div className="grid-right">
+										<div className="box-playbackIssues box-tbl">
+											<div className="head">
+												<div className="title">
+													<h2>Playback Issues</h2>
+													<div className="ui-tooltip">
+														<i>툴팁</i>
+														<div className="tooltip">tooltip</div>
+													</div>
+												</div>
+											</div>
+											<div className="ui-table-default">
+												<table>
+													<colgroup>
+														<col width={58}></col>
+														<col width={44}></col>
+														<col width={47}></col>
+														<col width={310}></col>
+													</colgroup>
+													<thead>
+														<tr>
+															<th scope="col">Date</th>
+															<th scope="col">Country</th>
+															<th scope="col">Platform</th>
+															<th scope="col">Issue Contents</th>
+														</tr>
+													</thead>
+													<tbody>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>Not playable Not playable</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+														<tr>
+															<td>23-07-07</td>
+															<td>1</td>
+															<td>11</td>
+															<td>
+																<span className="title">
+																	Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not
+																	playable
+																</span>
+															</td>
+														</tr>
+													</tbody>
+												</table>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+							<div className="grid-right">
+								<div className="box-imageIssues box-tbl">
+									<div className="head">
+										<div className="title">
+											<h2>Image Issues</h2>
+											<div className="ui-tooltip">
+												<i>툴팁</i>
+												<div className="tooltip">tooltip</div>
+											</div>
+										</div>
+									</div>
+									<div className="ui-table-default">
+										<table>
+											<colgroup>
+												<col width={58}></col>
+												<col width={44}></col>
+												<col width={47}></col>
+												<col width={310}></col>
+											</colgroup>
+											<thead>
+												<tr>
+													<th scope="col">Date</th>
+													<th scope="col">Country</th>
+													<th scope="col">Platform</th>
+													<th scope="col">Issue Contents</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>Not playable Not playable</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>Not playable Not playable</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+								<div className="box-secheduleIssues box-tbl">
+									<div className="head">
+										<div className="title">
+											<h2>Schedule Issues</h2>
+											<div className="ui-tooltip">
+												<i>툴팁</i>
+												<div className="tooltip">tooltip</div>
+											</div>
+										</div>
+									</div>
+									<div className="ui-table-default">
+										<table>
+											<colgroup>
+												<col width={58}></col>
+												<col width={44}></col>
+												<col width={47}></col>
+												<col width={310}></col>
+											</colgroup>
+											<thead>
+												<tr>
+													<th scope="col">Date</th>
+													<th scope="col">Country</th>
+													<th scope="col">Platform</th>
+													<th scope="col">Issue Contents</th>
+												</tr>
+											</thead>
+											<tbody>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>Not playable Not playable</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">Not playable Not playable Not playable Not playable Not playable</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>Not playable Not playable</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+												<tr>
+													<td>23-07-07</td>
+													<td>1</td>
+													<td>11</td>
+													<td>
+														<span className="title">
+															Not playable Not playable Not playable Not playable Not playable ot playable Not playable Not playable Not playable Not playable
+														</span>
+													</td>
+												</tr>
+											</tbody>
+										</table>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 export default Dashboard;
