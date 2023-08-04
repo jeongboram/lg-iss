cd4eb406e4fdf6fb23a1e7d90ef34f081162bb96
yckim <yongch.kim@gmail.com>
2023-08-04 14:07:28 +0900

    fix : chartjs 추가

--- src/components/layout/SideBar.js
+++ src/components/layout/SideBar.js
@@ -1,10 +1,66 @@
 import React from 'react';
+import {
+	Chart as ChartJS,
+	CategoryScale,
+	LinearScale,
+	PointElement,
+	LineElement,
+  } from "chart.js";
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
+  
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement);
+
+export const options = {
+	responsive: true,
+	plugins: {
+		legend: { 
+			position: 'top'
+		},
+		title: {
+			display: true,
+			text: 'Chart.js Line Chart'
+		},
+	},
+  };
+  
+  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  
+  export const data = {
+	labels,
+	datasets: [{
+		label: 'Dataset 1',
+		data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+		borderColor: 'rgb(255, 99, 132)',
+		backgroundColor: 'rgba(255, 99, 132, 0.5)',
+	},
+	{
+		label: 'Dataset 2',
+		data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+		borderColor: 'rgb(53, 162, 235)',
+		backgroundColor: 'rgba(53, 162, 235, 0.5)',
+	},
+	],
+  };
 
 function SideBar() {
 	const onSubmit = (e) => {
 		e.preventDefault();
 		console.log('on submit');
 	};
+
+	const data = {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+		datasets: [
+			{
+				label: "Second dataset",
+				data: [33, 25, 35, 51, 54, 76],
+				fill: false,
+				borderColor: "#742774"
+			}
+		]
+	};
+
 	return (
 		<>
 			<div className="sidebar-section">
@@ -56,7 +112,9 @@ function SideBar() {
 										<h3>
 											Status of issues by time zone <span>(Based on UTC)</span>
 										</h3>
-										<div className="chart-wrap">chart area</div>
+										<div className="chart-wrap">
+										<Line options={options} data={data} />
+										</div>
 									</div>
 								</div>
 							</div>
